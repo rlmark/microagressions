81c1796e55d399f4dfd839764c0dd3db3378a36c
Rebecca Mark <rebecca.lmark@gmail.com>
2014-11-01 14:05:34 -0700

    created welcome controller, organized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,67 @@
 Rails.application.routes.draw do
+
+  # HOME PAGE
+  get 'welcome/index', to: 'welcome#index', as: :root
+
+  # POSTS ROUTES
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/update'
+
+  get 'posts/edit'
+
+  get 'posts/destroy'
+
+  get 'posts/show'
+
+  get 'posts/index', to: 'posts#index', as: :posts
+
+  # CATEGORIES ROUTES
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/edit'
+
+  get 'categories/destroy'
+
+  get 'categories/show'
+
+  get 'categories/index'
+
+  # COMMENTS ROUTES
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'comments/update'
+
+  get 'comments/edit'
+
+  get 'comments/destroy'
+
+  get 'comments/show'
+
+  get 'comments/index'
+
+  get 'post/new'
+
+  get 'post/create'
+
+  get 'post/update'
+
+  get 'post/edit'
+
+  get 'post/destroy'
+
+  get 'post/show'
+
+  get 'post/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
